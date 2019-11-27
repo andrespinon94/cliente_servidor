b893385a46df282c40e173ece622db1fa2b83a47
andres <andres_constantino_pinon_apps1ma1819@cev.com>
2019-11-27 14:37:09 +0100

    inicio de peticiones get AJAX

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 
-document.getElementById("btn").onclick = function(){register_user()};
+document.getElementById("btn_registrar").onclick = function(){register_user()};
+document.getElementById("btn_mostrar_usuarios").onclick = function(){show_users()};
+document.getElementById("btn_mostrar_libros").onclick = function(){show_books()};
 
 function register_user() { 
 
@@ -10,10 +12,35 @@ var email = document.getElementById("Email").value ;
 var password = document.getElementById("Password").value ;
 var datos_usuario = {'name':name,'email':email,'password':password};
 
-
 $.ajax(URL,{
     type: 'POST',
     data: datos_usuario
+}).done(function() {
+    alert( "Usuario Registrado");
+  });
+}
+
+
+function show_users(){
+var URL = "http://localhost:8888/biblioteca/public/index.php/api/show";
+
+$.ajax(URL,{
+    type: 'GET',
+    success:function(respuesta){
+        console.log(respuesta);
+    }
+});
+}
+
+function show_books(){
+
+var URL = "http://localhost:8888/biblioteca/public/index.php/api/books";
+
+$.ajax(URL,{
+    type: 'GET',
+    success:function(respuesta){
+        console.log(respuesta);
+    }
 });
 
 }
