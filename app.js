39981eb8b66bc204b524bc999879bb87752b732d
andres <andres_constantino_pinon_apps1ma1819@cev.com>
2019-11-28 14:37:08 +0100

    token recibido y almasenado en sesion

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
-
 document.getElementById("btn_registrar").onclick = function(){register_user()};
 document.getElementById("btn_mostrar_usuarios").onclick = function(){show_users()};
 document.getElementById("btn_mostrar_libros").onclick = function(){show_books()};
+document.getElementById("btn_login").onclick = function(){login()};
 
 function register_user() { 
 
@@ -20,6 +20,34 @@ $.ajax(URL,{
   });
 }
 
+function login(){
+var URL = "http://localhost:8888/biblioteca/public/index.php/api/login"
+var email = document.getElementById("email_login").value ;
+var password = document.getElementById("password_login").value ;
+var datos_login = {'email':email,'password':password};
+
+$.ajax(URL,{
+    type:'POST',
+    data: datos_login,
+    success: function(response) {
+        console.log('success')
+        sessionStorage.setItem('token', response.token);
+      }
+}).done(function(){
+    alert("logged in");
+  });
+}
+
+/*function get_token(){
+
+    $.ajax({
+        url: "aqui quizas valla la siguiente ruta que regresa un token???",
+        type: 'GET',
+        // Fetch the stored token from localStorage and set in the header
+        headers: {"Authorization": localStorage.getItem('token')}
+      });
+}*/
+
 
 function show_users(){
 var URL = "http://localhost:8888/biblioteca/public/index.php/api/show";
@@ -28,6 +56,16 @@ $.ajax(URL,{
     type: 'GET',
     success:function(respuesta){
         console.log(respuesta);
+        var userlist = document.getElementById("user_list").value;
+        $.each(respuesta.data,function(index,user){
+            userlist.append(
+
+            '<div>'
+            + '<p>' + user.name +'</p>'
+            + '<p>' + user.email +'</p>'
+            + '</div>'
+            );
+        });
     }
 });
 }
